a08dc34ea89dcaf56d23d58f0552b437e4fb727a
undone37 <undone37>
2025-06-15 15:54:21 +0200

    Speed up MQTT client startup

--- Code/src/main.cpp
+++ Code/src/main.cpp
@@ -37,7 +37,7 @@ void setup()
     periodicTimer.attach(periodicTimerInterval, []
                          { periodicTimerFlag = true; });
     // delayed mqtt start
-    startComplete.attach(61, []
+    startComplete.attach(20, []
                          { if(useMqtt) enableMqtt = true; startComplete.detach(); });
     // update webpage every 2 seconds. (will also be updated on state changes)
     updateWSTimer.attach(2.0, []
