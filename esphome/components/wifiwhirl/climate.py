8633ca116ca6c385a7f7975e0eec05ce3dd46652
Michael Finest <finest@users.noreply.github.com>
2026-05-05 13:24:48 +0200

    Fix ESPHome 2026.4 climate schema API

--- esphome/components/wifiwhirl/climate.py
+++ esphome/components/wifiwhirl/climate.py
@@ -1,7 +1,7 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import climate
-from esphome.const import CONF_ID, CONF_NAME
+from esphome.const import CONF_ID
 
 from . import WifiWhirlComponent
 
@@ -12,10 +12,10 @@ CONF_WIFIWHIRL_ID = "wifiwhirl_id"
 wifiwhirl_ns = cg.esphome_ns.namespace("wifiwhirl")
 WifiWhirlClimate = wifiwhirl_ns.class_("WifiWhirlClimate", climate.Climate)
 
-CONFIG_SCHEMA = climate.CLIMATE_SCHEMA.extend(
+# ESPHome 2026.4+: CLIMATE_SCHEMA was replaced by climate.climate_schema(...)
+CONFIG_SCHEMA = climate.climate_schema(WifiWhirlClimate).extend(
     {
         cv.GenerateID(CONF_WIFIWHIRL_ID): cv.use_id(WifiWhirlComponent),
-        cv.GenerateID(): cv.declare_id(WifiWhirlClimate),
     }
 )
 
